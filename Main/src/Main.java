db8ab6fff312a606751b2c69f59df6302a336e2e
luca16s <luca.gian16@outlook.com>
2017-11-26 23:45:34 -0200

    implemented the check on stack for "(), [], {}"

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -7,27 +7,61 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        boolean valido = true;
         String mathExpression = scanner.nextLine();
-
-        Stack s = inicPilha();
+        Object auxVariable = "";
+        Stack<String> s = inicPilha();
 
         for (int i = 0; i < mathExpression.length(); i++) {
-            push(s, mathExpression.substring(i, i + 1));
+
+            if (mathExpression.substring(i, i + 1).equals("(")
+                    || mathExpression.substring(i, i + 1).equals("[")
+                    || mathExpression.substring(i, i + 1).equals("{")) {
+                push(s, mathExpression.substring(i, i + 1));
+            } else if (mathExpression.substring(i, i + 1).equals(")")
+                    || mathExpression.substring(i, i + 1).equals("]")
+                    || mathExpression.substring(i, i + 1).equals("}")) {
+                push(s, mathExpression.substring(i, i + 1));
+            }
+
+            if (mathExpression.substring(i, i + 1).equals(")")
+                    || mathExpression.substring(i, i + 1).equals("]")
+                    || mathExpression.substring(i, i + 1).equals("}")) {
+                if (pilhaVazia(s)) {
+                    valido = false;
+                } else {
+                    auxVariable = pop(s);
+                    if (auxVariable.equals(mathExpression.substring(i, i))) {
+                        valido = false;
+                    }
+                }
+            }
         }
 
+        if (!pilhaVazia(s)) {
+            valido = false;
+        }
+        if (valido) {
+            out.println("Expressão válida");
+        } else {
+            out.println("Expressão inválida");
+        }
+    }
 
-        out.println(s);
+    private static boolean pilhaVazia(Stack s) {
+        return s.empty();
     }
 
     private static Stack inicPilha() {
-        return new Stack();
+        return new Stack<String>();
     }
 
     private static void push(Stack s, String a) {
         s.push(a);
     }
 
-    public void pop(Stack s) {
-        s.pop();
+    public static String pop(Stack s) {
+        String local = (String) s.pop();
+        return local;
     }
 }
